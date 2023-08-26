3a8d33dde3b33e5777c8cb498f90da0190d8827e
Emill <lemito66@gmail.com>
2023-08-25 20:57:24 -0500

    input

---
+++ Python/input.py
@@ -0,0 +1,6 @@
+number = input('Ingresa el primer número: ')
+print('El número ingresado es: ', number)
+number_two = input('Ingresa el segundo número: ')
+print('El número ingresado es: ', number_two)
+
+print(f'La suma de los números es: {int(number) + int(number_two)}')
