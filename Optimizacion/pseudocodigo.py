a7f504333c8144672ae4593de551c533c0d693dc
Emill <lemito66@gmail.com>
2024-03-04 20:22:23 -0500

    problema inicial con pseudocodigo pasado a código

---
+++ Optimizacion/pseudocodigo.py
@@ -0,0 +1,8 @@
+def discotec_entrance(age):
+    if age > 18:
+        print("Contenido de adulto")
+        if age > 65:
+            print('Contenido de mayor de edad')
+            
+            
+print(discotec_entrance(70))
